f098070bf4c05f73f9445169b8091f8262a24e7e
edgarosovel <edgarov@live.com.mx>
2016-09-25 00:46:35 -0500

    link

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 	ini_set('display_errors', 'on');
 
 	//define( 'URL' ,"http://127.0.0.1/salud/");
-	define( 'URL' ,"http://localhost:9999/newmvc/");
+	define( 'URL' ,"http://localhost:9999/ProyectoCD/");
 
 	define( 'CSS' , URL."public/css/");
 	define( 'JS' , URL."public/js/" );
